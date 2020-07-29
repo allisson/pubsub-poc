4110da38ff2ab956521ef16b1d50e2a1cbefb097
Allisson Azevedo <allisson@gmail.com>
2020-07-29 13:37:38 -0300

    Make create topic and subscription more simple

--- subscription.go
+++ subscription.go
@@ -7,125 +7,25 @@ import (
 	"go.uber.org/zap"
 )
 
-func getSubscription(ctx context.Context, client *pubsub.Client, subID, topicID string, subConfig pubsub.SubscriptionConfig) (*pubsub.Subscription, error) {
-	// Check if subscription exists
-	sub := client.Subscription(subID)
-	ok, err := sub.Exists(ctx)
+// CreateSubscription ...
+func CreateSubscription(ctx context.Context, projectID, subID string, subConfig pubsub.SubscriptionConfig) (*pubsub.Subscription, error) {
+	client, err := pubsub.NewClient(context.Background(), projectID)
 	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	AlreadyExistsErr := "rpc error: code = AlreadyExists desc = Subscription already exists"
+	sub, err := client.CreateSubscription(ctx, subID, subConfig)
+	if err != nil {
+		if err.Error() == AlreadyExistsErr {
+			sub := client.Subscription(subID)
+			return sub, nil
+		}
+		logger.Error("subscription_create", zap.String("subscription_id", subID), zap.Error(err))
 		return sub, err
 	}
-	if !ok {
-		return sub, ErrSubscriptionNotFound
-	}
 
+	logger.Info("subscription_created", zap.String("subscription_id", subID))
 	return sub, nil
-}
-
-// ConsumeFunc ...
-type ConsumeFunc func(ctx context.Context, msg *pubsub.Message) error
-
-// Subscription ...
-type Subscription interface {
-	Create(ctx context.Context) (*pubsub.Subscription, error)
-	Consume(ctx context.Context, fn ConsumeFunc, maxOutstandingMessages uint) error
-}
-
-type subscription struct {
-	id        string
-	topicID   string
-	subConfig pubsub.SubscriptionConfig
-	client    *pubsub.Client
-}
-
-func (s *subscription) Create(ctx context.Context) (*pubsub.Subscription, error) {
-	// Get topic
-	topic, err := getTopic(ctx, s.client, s.topicID)
-	if err != nil {
-		return &pubsub.Subscription{}, err
-	}
-
-	// Create subscription
-	s.subConfig.Topic = topic
-	AlreadyExistsErr := "rpc error: code = AlreadyExists desc = Subscription already exists"
-	sub, err := s.client.CreateSubscription(ctx, s.id, s.subConfig)
-	if err != nil {
-		if err.Error() != AlreadyExistsErr {
-			logger.Error("subscription_create", zap.String("subscription_id", s.id), zap.Error(err))
-			return sub, err
-		}
-	}
-
-	logger.Info("subscription_created", zap.String("subscription_id", s.id))
-	return sub, nil
-}
-
-func (s *subscription) Consume(ctx context.Context, fn ConsumeFunc, maxOutstandingMessages uint) error {
-	// Get subscription
-	sub, err := getSubscription(ctx, s.client, s.id, s.topicID, s.subConfig)
-	if err != nil {
-		return err
-	}
-
-	logger.Info(
-		"subscription_consume_started",
-		zap.String("subscription_id", s.id),
-		zap.String("topic_id", s.topicID),
-		zap.Uint("max_outstanding_messages", maxOutstandingMessages),
-	)
-
-	err = sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
-		logger.Info(
-			"subscription_consume_handle_message_started",
-			zap.String("subscription_id", s.id),
-			zap.String("topic_id", s.topicID),
-			zap.Uint("max_outstanding_messages", maxOutstandingMessages),
-			zap.String("message_id", msg.ID),
-		)
-
-		if err := fn(ctx, msg); err != nil {
-			logger.Error(
-				"subscription_consume_handle_message_error",
-				zap.String("subscription_id", s.id),
-				zap.String("topic_id", s.topicID),
-				zap.Uint("max_outstanding_messages", maxOutstandingMessages),
-				zap.String("message_id", msg.ID),
-				zap.Reflect("message", msg),
-				zap.Error(err),
-			)
-			msg.Nack()
-			return
-		}
-
-		msg.Ack()
-		logger.Info(
-			"subscription_consume_handle_message_completed",
-			zap.String("subscription_id", s.id),
-			zap.String("topic_id", s.topicID),
-			zap.Uint("max_outstanding_messages", maxOutstandingMessages),
-			zap.String("message_id", msg.ID),
-		)
-	})
-
-	if err != context.Canceled {
-		logger.Error(
-			"subscription_consume_started",
-			zap.String("subscription_id", s.id),
-			zap.String("topic_id", s.topicID),
-			zap.Uint("max_outstanding_messages", maxOutstandingMessages),
-			zap.Error(err),
-		)
-		return err
-	}
-
-	return nil
-}
-
-// NewSubscription ...
-func NewSubscription(id, topicID string, subConfig pubsub.SubscriptionConfig, client *pubsub.Client) Subscription {
-	return &subscription{
-		id:        id,
-		topicID:   topicID,
-		subConfig: subConfig,
-		client:    client,
-	}
 }
--- topic_test.go
+++ topic_test.go
@@ -4,51 +4,26 @@ import (
 	"context"
 	"os"
 	"testing"
-	"time"
 
-	"cloud.google.com/go/pubsub"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTopic(t *testing.T) {
+func TestCreateTopic(t *testing.T) {
 	// Use pubsub emulator
 	os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
 
-	// Pubsub client
-	projectID := "my-project-id"
-	client, err := pubsub.NewClient(context.Background(), projectID)
+	// Fixtures
+	projectID := "my-project-id-2"
+	topicID := "my-topic-id-2"
+	ctx := context.Background()
+
+	// Create topic
+	topic, err := CreateTopic(ctx, projectID, topicID)
 	assert.Nil(t, err)
+	assert.Equal(t, topicID, topic.ID())
 
-	t.Run("Publish", func(t *testing.T) {
-		ctx := context.Background()
-		topicID := "topic-id-1"
-		topic := NewTopic(topicID, client)
-
-		// Create topic
-		_, err := topic.Create(ctx)
-		assert.Nil(t, err)
-
-		// Publish to topic
-		attributes := map[string]string{"attr1": "attr1", "attr2": "attr2"}
-		id, err := topic.Publish(ctx, []byte(`{"payload": true}`), attributes)
-		assert.Nil(t, err)
-		assert.NotEqual(t, "", id)
-	})
-
-	t.Run("Publish with timeout", func(t *testing.T) {
-		expectedError := "rpc error: code = DeadlineExceeded desc = context deadline exceeded"
-		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Microsecond)
-		defer cancel()
-		topicID := "topic-id-1"
-		topic := NewTopic(topicID, client)
-
-		// Create topic
-		_, err := topic.Create(context.Background())
-		assert.Nil(t, err)
-
-		// Publish to topic
-		attributes := map[string]string{"attr1": "attr1", "attr2": "attr2"}
-		_, err = topic.Publish(ctx, []byte(`{"payload": true}`), attributes)
-		assert.Equal(t, expectedError, err.Error())
-	})
+	// Create again to force already exists error
+	topic, err = CreateTopic(ctx, projectID, topicID)
+	assert.Nil(t, err)
+	assert.Equal(t, topicID, topic.ID())
 }
